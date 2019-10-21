8225a38f0ce88975fb0042245b28d1732a901095
Leggett <leggett@gmail.com>
2019-10-20 22:33:53 -0700

    Removing dev folder
    I should use branches instead

--- gmail/stable/options.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function(){
-	if (window.navigator.platform.indexOf('Mac') >= 0) {
-		document.getElementById('systemKey1').innerText = 'Cmd';
-		document.getElementById('systemKey2').innerText = 'Cmd';
-	}	
-}, false)
